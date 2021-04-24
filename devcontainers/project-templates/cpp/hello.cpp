25a5c9953cc84f62d61a1f0bcc3857803dbd61cb
TylerShopp <tylershopp@comcast.net>
2021-04-24 11:08:56 -0400

    Addinng modified c++ container

--- devcontainers/project-templates/cpp/hello.cpp
+++ devcontainers/project-templates/cpp/hello.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 #include <vector>
 
+using std::cout;
+using std::endl;
+using std::vector;
+
+using namespace std;
+
 int
 main ()
+
 {
-  std::vector<int> v;
+  vector<int> v;
   for (int i : {5, 4, 3, 2, 1})
   {
     v.push_back (i);
   }
 
-  for (auto val : v)
-  {
-    std::cout << val << ' ';
+   int a = 3;
+ 
+   // check the condition
+   if( a != 6 )
+   {
+     // if number does not equal 6
+     cout << "number does not equal what i'm thinking;" << endl;
+   }
+   else
+   {
+     // if condition is false then print the following
+     cout << "This is the number I was thinking;" << endl;
+   }
+   cout << "The number I was thinking was:" << a << endl;
+
+   for (auto val : v)
+   {
+     cout << val << ' ';
   }
-  std::cout << "\nHello, World!" << std::endl;
+  cout << "\nHello, World!" << endl;
 
   return 0;
 }
